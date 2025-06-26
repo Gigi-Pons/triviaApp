0b17d8dd3fa1b4927aa1db732d25077539171be2
Gigi-Pons <grecia.atx@gmail.com>
2025-06-26 00:04:55 -0600

    Populated the options table

---
+++ src/main/resources/db/migration/V6__insert_into_options_table.sql
@@ -0,0 +1,80 @@
+INSERT INTO options (question_id, label, text)
+VALUES ((SELECT id FROM questions WHERE text = 'What is the largest organ in the human body?' LIMIT 1), 'A', 'The liver');
+INSERT INTO options (question_id, label, text)
+VALUES ((SELECT id FROM questions WHERE text = 'What is the largest organ in the human body?' LIMIT 1), 'B', 'The lungs');
+INSERT INTO options (question_id, label, text)
+VALUES ((SELECT id FROM questions WHERE text = 'What is the largest organ in the human body?' LIMIT 1), 'C', 'The intestines');
+INSERT INTO options (question_id, label, text)
+VALUES ((SELECT id FROM questions WHERE text = 'What is the largest organ in the human body?' LIMIT 1), 'D', 'The skin');
+
+INSERT INTO options (question_id, label, text)
+VALUES ((SELECT id FROM questions WHERE text = 'What is the name of the bone that protects your brain?' LIMIT 1), 'A', 'Ulna');
+INSERT INTO options (question_id, label, text)
+VALUES ((SELECT id FROM questions WHERE text = 'What is the name of the bone that protects your brain?' LIMIT 1), 'B', 'Cerebellum');
+INSERT INTO options (question_id, label, text)
+VALUES ((SELECT id FROM questions WHERE text = 'What is the name of the bone that protects your brain?' LIMIT 1), 'C', 'The skull');
+INSERT INTO options (question_id, label, text)
+VALUES ((SELECT id FROM questions WHERE text = 'What is the name of the bone that protects your brain?' LIMIT 1), 'D', 'Vertebrae');
+
+INSERT INTO options (question_id, label, text)
+VALUES ((SELECT id FROM questions WHERE text = 'Which part of the brain controls balance and coordination?' LIMIT 1), 'A', 'The cerebellum');
+INSERT INTO options (question_id, label, text)
+VALUES ((SELECT id FROM questions WHERE text = 'Which part of the brain controls balance and coordination?' LIMIT 1), 'B', 'The hypothalamus');
+INSERT INTO options (question_id, label, text)
+VALUES ((SELECT id FROM questions WHERE text = 'Which part of the brain controls balance and coordination?' LIMIT 1), 'C', 'The pituitary gland');
+INSERT INTO options (question_id, label, text)
+VALUES ((SELECT id FROM questions WHERE text = 'Which part of the brain controls balance and coordination?' LIMIT 1), 'D', 'The frontal lobe');
+
+INSERT INTO options (question_id, label, text)
+VALUES ((SELECT id FROM questions WHERE text = 'What type of organism make up the oldest known fossil?' LIMIT 1), 'A', 'Plants');
+INSERT INTO options (question_id, label, text)
+VALUES ((SELECT id FROM questions WHERE text = 'What type of organism make up the oldest known fossil?' LIMIT 1), 'B', 'Dust');
+INSERT INTO options (question_id, label, text)
+VALUES ((SELECT id FROM questions WHERE text = 'What type of organism make up the oldest known fossil?' LIMIT 1), 'C', 'Algae');
+INSERT INTO options (question_id, label, text)
+VALUES ((SELECT id FROM questions WHERE text = 'What type of organism make up the oldest known fossil?' LIMIT 1), 'D', 'Bacteria');
+
+INSERT INTO options (question_id, label, text)
+VALUES ((SELECT id FROM questions WHERE text = 'What is the name of the green pigment found in plants?' LIMIT 1), 'A', 'Microbe');
+INSERT INTO options (question_id, label, text)
+VALUES ((SELECT id FROM questions WHERE text = 'What is the name of the green pigment found in plants?' LIMIT 1), 'B', 'Algae');
+INSERT INTO options (question_id, label, text)
+VALUES ((SELECT id FROM questions WHERE text = 'What is the name of the green pigment found in plants?' LIMIT 1), 'C', 'Chlorophyll');
+INSERT INTO options (question_id, label, text)
+VALUES ((SELECT id FROM questions WHERE text = 'What is the name of the green pigment found in plants?' LIMIT 1), 'D', 'Eukaryotic');
+
+INSERT INTO options (question_id, label, text)
+VALUES ((SELECT id FROM questions WHERE text = 'What is the process called when a caterpillar transforms into a butterfly?' LIMIT 1), 'A', 'Organism');
+INSERT INTO options (question_id, label, text)
+VALUES ((SELECT id FROM questions WHERE text = 'What is the process called when a caterpillar transforms into a butterfly?' LIMIT 1), 'B', 'Microbiology');
+INSERT INTO options (question_id, label, text)
+VALUES ((SELECT id FROM questions WHERE text = 'What is the process called when a caterpillar transforms into a butterfly?' LIMIT 1), 'C', 'Homeostasis');
+INSERT INTO options (question_id, label, text)
+VALUES ((SELECT id FROM questions WHERE text = 'What is the process called when a caterpillar transforms into a butterfly?' LIMIT 1), 'D', 'Metamorphosis');
+
+INSERT INTO options (question_id, label, text)
+VALUES ((SELECT id FROM questions WHERE text = 'If you mix all colors of light together, what color do you get?' LIMIT 1), 'A', 'White');
+INSERT INTO options (question_id, label, text)
+VALUES ((SELECT id FROM questions WHERE text = 'If you mix all colors of light together, what color do you get?' LIMIT 1), 'B', 'Black');
+INSERT INTO options (question_id, label, text)
+VALUES ((SELECT id FROM questions WHERE text = 'If you mix all colors of light together, what color do you get?' LIMIT 1), 'C', 'Yellow');
+INSERT INTO options (question_id, label, text)
+VALUES ((SELECT id FROM questions WHERE text = 'If you mix all colors of light together, what color do you get?' LIMIT 1), 'D', 'Grey');
+
+INSERT INTO options (question_id, label, text)
+VALUES ((SELECT id FROM questions WHERE text = 'What is the force that resists motion when two solid surfaces rub against each other?' LIMIT 1), 'A', 'Acceleration');
+INSERT INTO options (question_id, label, text)
+VALUES ((SELECT id FROM questions WHERE text = 'What is the force that resists motion when two solid surfaces rub against each other?' LIMIT 1), 'B', 'Friction');
+INSERT INTO options (question_id, label, text)
+VALUES ((SELECT id FROM questions WHERE text = 'What is the force that resists motion when two solid surfaces rub against each other?' LIMIT 1), 'C', 'Gravity');
+INSERT INTO options (question_id, label, text)
+VALUES ((SELECT id FROM questions WHERE text = 'What is the force that resists motion when two solid surfaces rub against each other?' LIMIT 1), 'D', 'Momentum');
+
+INSERT INTO options (question_id, label, text)
+VALUES ((SELECT id FROM questions WHERE text = 'Where does sound travel faster?' LIMIT 1), 'A', 'Space');
+INSERT INTO options (question_id, label, text)
+VALUES ((SELECT id FROM questions WHERE text = 'Where does sound travel faster?' LIMIT 1), 'B', 'Air');
+INSERT INTO options (question_id, label, text)
+VALUES ((SELECT id FROM questions WHERE text = 'Where does sound travel faster?' LIMIT 1), 'C', 'Water');
+INSERT INTO options (question_id, label, text)
+VALUES ((SELECT id FROM questions WHERE text = 'Where does sound travel faster?' LIMIT 1), 'D', 'Room');
