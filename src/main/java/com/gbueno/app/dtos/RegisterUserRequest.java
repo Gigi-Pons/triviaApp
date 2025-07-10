e17cc4a25b0d9c073b1b6e1fda82dfd43ee20262
Gigi-Pons <grecia.atx@gmail.com>
2025-07-10 11:37:00 -0600

    Added starter validation to pom file to do basic validation in RegisterUserRequest DTO

--- src/main/java/com/gbueno/app/dtos/RegisterUserRequest.java
+++ src/main/java/com/gbueno/app/dtos/RegisterUserRequest.java
@@ -1,9 +1,20 @@
 package com.gbueno.app.dtos;
 
-public class RegisterUserRequest {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
+public class RegisterUserRequest {
+    @NotBlank(message = "Name is required")
+    @Size(max = 255, message = "Name must be less than 255 characters")
     private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be valid")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 25, message = "Password must be between 6 to 25 characters long")
     private String password;
 }
 
