d23c94a20da037a845eabdb2449bde467c5a328e
Gigi-Pons <grecia.atx@gmail.com>
2025-07-10 11:28:01 -0600

    Created the User entity

---
+++ src/main/java/com/gbueno/app/entities/User.java
@@ -0,0 +1,28 @@
+package com.gbueno.app.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+}
